507474875e132f2355dc4155410ca7b2a991ffe0
diego1891 <diegobronc1508@gmail.com>
2023-05-21 21:53:44 -0300

    Maneja bien el array

--- src/Components/houses/gryffindor/gryffindor.jsx
+++ src/Components/houses/gryffindor/gryffindor.jsx
@@ -1,23 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect,useState } from "react";
+import { fetchAllCharacters } from "../../../ApiService/Api";
 
 const Gryffindor = ({ character }) => {
-    if (!character) {
-      return <p>BIENVENIDO</p>;
-    }
-    return (
-      <div className="row">
-        {character.map((character,index)=>(
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      var data = await fetchAllCharacters();
+      console.log(data);
+      setCharacters(data);
+    };
+    getData();
+  }, []);
+
+  if (!characters) {
+    return <p>BIENVENIDO</p>;
+  }
+  return (
+    <div className="row">
+      {characters.map((character, index) => (
         <div key={index} className="col">
           <div className="card">
-            <img src={character.image} alt="">
-
-            </img>
-
+            <img src={character.image} alt=""></img>
           </div>
-
         </div>
-        ))}
-      </div>
-    );
-  };
-  export default Gryffindor;
+      ))}
+    </div>
+  );
+};
+export default Gryffindor;
